b2c35096ffc85d563b2784a064113d0f9b97af38
Ilya <ilyanek@mail.ru>
2021-09-18 21:38:29 +0400

    menu_foot

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,30 @@
 let gulp = require('gulp'),
-    sass = require('gulp-sass')(require('sass'));
+    sass = require('gulp-sass')(require('sass')),
+    browserSync = require('browser-sync');
 
 gulp.task('scss', function() {
     return gulp.src('app/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'));
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream:true}));
 });
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+});
+
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/**.scss', gulp.parallel('scss'));
+    gulp.watch('app/*html', gulp.parallel('html'));
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
