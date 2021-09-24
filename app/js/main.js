a7a88da5e6f4721e7354c6c3af32a835c988aa95
Ilya <ilyanek@mail.ru>
2021-09-24 23:38:18 +0400

    catalog_setting

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,33 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const btn1 = document.querySelector('.carousel__btn-left'),
-        btn2 = document.querySelector('.carousel__btn-right'),
-        img = document.querySelectorAll('.carousel__img');
+    // const btn1 = document.querySelector('.carousel__btn-left'),
+    //     btn2 = document.querySelector('.carousel__btn-right'),
+    //     img = document.querySelectorAll('.carousel__img');
 
-    btn1.addEventListener('click', function () {
-        img.classList.toggle('carousel__img_active');
-    });
+    // btn1.addEventListener('click', function () {
+    //     img.classList.toggle('carousel__img_active');
+    // });
+
+
+    //catalog
+
+    // let catalogItem = document.querySelectorAll('.catalog__item'),
+    //     catDimensions = document.querySelector('.catalog__dimensions');        
+    //     catalogItem.forEach(item => {
+    //         item.addEventListener('click', (item) => {
+
+    //             catDimensions.style.display = 'block';
+    //             catDimensions.style.opacity = '1';
+            
+    //         });
+
+    //     })
+
+
+        // menuItem.forEach(item => {
+        //     item.addEventListener('click', () => {
+        //         hamburger.classList.toggle('hamburger_active');
+        //         menu.classList.toggle('menu_active');
+        //     })
+        // })
+
 });
